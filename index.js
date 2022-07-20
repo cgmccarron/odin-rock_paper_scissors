68535a11784ecbda9b5399087bc226fd6068a025
cgmccarr <cgmccarr@gmail.com>
2022-07-19 23:45:05 -0500

    confirming i comitted

--- index.js
+++ index.js
@@ -9,3 +9,60 @@ const getComputerChoice = () => {
       return "rock";
   }
 };
+
+const playRound = (compChoice, playerChoice) => {
+  switch (playerChoice) {
+    case "scissors":
+      switch (compChoice) {
+        case "scissors":
+          return "TIE";
+        case "paper":
+          return "WIN";
+        case "rock":
+          return "LOSS";
+      }
+    case "paper":
+      switch (compChoice) {
+        case "scissors":
+          return "LOSS";
+        case "paper":
+          return "TIE";
+        case "rock":
+          return "WIN";
+      }
+    case "rock":
+      switch (compChoice) {
+        case "scissors":
+          return "WIN";
+        case "paper":
+          return "LOSS";
+        case "rock":
+          return "TIE";
+      }
+  }
+};
+
+function game() {
+  let player = 0;
+  let computer = 0;
+
+  while (player < 5 && computer < 5) {
+    let playerChoice = prompt("What your input");
+    playerChoice = playerChoice.toLowerCase();
+    let compChoice = getComputerChoice();
+    let result = playRound(playerChoice, compChoice);
+
+    if (result === "WIN") {
+      player++;
+    } else if (result === "LOSS") {
+      computer++;
+    }
+
+    console.log(result);
+  }
+
+  console.log("Your score : " + player);
+  console.log("Computer score : " + computer);
+}
+
+game();
